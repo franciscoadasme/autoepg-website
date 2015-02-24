9e84dc2c4a463ec11c0909cc7f01d9fe32b49d43
Francisco Adasme <francisco.adasme@gmail.com>
2015-02-24 17:19:36 -0300

    Changed old links and text to use A2EPG

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -2,13 +2,17 @@ require 'digest/md5'
 require 'active_support/core_ext/integer/inflections'
 
 helpers do
-  def gravatar_for(email, params = {})
-    hash = Digest::MD5.hexdigest(email.downcase)
-    image_src = "http://www.gravatar.com/avatar/#{hash}?#{URI.encode_www_form params}"
-  end
-
   def aws_link_to(content)
     File.join(settings.aws_base_url, content)
+  end
+
+  def gravatar_for(email, params = {})
+    hash = Digest::MD5.hexdigest(email.downcase)
+    "http://www.gravatar.com/avatar/#{hash}?#{URI.encode_www_form params}"
+  end
+
+  def tag(kind)
+    %Q{<span class="#{kind} tag">#{kind}</span>}
   end
 
   def version_tag(version, build, date)
@@ -19,9 +23,5 @@ helpers do
         <span class="sep">;</span>
         <span class="date">#{date.strftime(date_format)}</span>
       </h2>}
-  end
-
-  def tag(kind)
-    %Q{<span class="#{kind} tag">#{kind}</span>}
   end
 end
--- web.rb
+++ web.rb
@@ -3,17 +3,17 @@ require 'sass'
 require "#{File.dirname(__FILE__)}/lib/helpers.rb"
 
 configure do
-  set :version, 'Version 0.9.11 Build 124 (Aug 2014)'
+  set :version, 'Version 0.9.12 Build 125 (Dec 2014)'
   set :aws_base_url, 'https://s3.amazonaws.com/autoepg/'
+end
+
+get '/css/stylesheet.css' do
+  content_type 'text/css', charset: 'utf-8'
+  scss :"../assets/css/stylesheet"
 end
 
 get '/' do
   erb :index
-end
-
-get '/css/stylesheet.css' do
-  content_type 'text/css', :charset => 'utf-8'
-  scss :"../assets/css/stylesheet"
 end
 
 get '/download/windows' do
@@ -25,23 +25,15 @@ get '/download/mac' do
 end
 
 get '/download/linux' do
-  redirect aws_link_to('a2epg.tar.gz')
+  redirect aws_link_to('A2EPG.tar.gz')
 end
 
 get '/download/user-manual' do
-  redirect aws_link_to('autoepg-user-manual.pdf')
-end
-
-get '/download/benchmark-toolset' do
-  redirect aws_link_to('autoepg-benchmark-toolset.zip')
+  redirect aws_link_to('a2epg-user-manual.pdf')
 end
 
 get '/download/post-analysis' do
-  redirect aws_link_to('autoepg-post-analysis.zip')
-end
-
-get %r{/datasets/(\w+)} do
-  redirect aws_link_to("autoepg-#{params[:captures][0]}-data-set.zip")
+  redirect aws_link_to('a2epg-post-analysis.py')
 end
 
 get '/release-notes' do
@@ -50,4 +42,8 @@ end
 
 get '/benchmark' do
   erb :benchmark
+end
+
+get %r{/datasets/(\w+)} do
+  redirect aws_link_to("a2epg_#{params[:captures][0]}_dataset.zip")
 end
